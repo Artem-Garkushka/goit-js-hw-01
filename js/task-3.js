4891115aea12341207bfa1f26d1c2f1d02570e63
Artem Garkushka <Artem.Garkushka@gmail.com>
2024-09-24 16:07:07 +0300

    js

--- js/task-3.js
+++ js/task-3.js
@@ -3,15 +3,5 @@ function getElementWidth(content, padding, border){
     return totalPadding;
 }
 console.log(getElementWidth("50px", "8px", "4px"));
-
-function getElementWidth(content, padding, border){
-    const totalPadding = Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2;
-    return totalPadding;
-}
 console.log(getElementWidth("60px", "12px", "8.5px"));
-
-function getElementWidth(content, padding, border){
-    const totalPadding = Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2;
-    return totalPadding;
-}
 console.log(getElementWidth("200px", "0px", "0px"));
