5cc0ac5951a9454e9cc4538caf4fb8587700137f
Artem Garkushka <Artem.Garkushka@gmail.com>
2024-09-24 13:56:58 +0300

    js

--- js/task-2.js
+++ js/task-2.js
@@ -8,7 +8,7 @@ function getShippingMessage(country, price, deliveryFee){
     const totalPrice = price + deliveryFee;
     return `Shipping to ${country} will cost ${totalPrice} credits`;
 }
-console.log(getShippingMessage("Germane", 80, 20));
+console.log(getShippingMessage("Germany", 80, 20));
 
 function getShippingMessage(country, price, deliveryFee){
     const totalPrice = price + deliveryFee;
